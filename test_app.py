89137c6a38ea8cbed56c92235160e71db2741bae
ShivamGoyal1507 <2023aib1016@iitrpr.ac.in>
2026-03-10 20:02:26 +0530

    fixing test.py

--- test_app.py
+++ test_app.py
@@ -8,5 +8,5 @@ def test_home():
     assert response.status_code == 200
 
 def test_prediction():
-    response = client.post("/predict", json={"features":[3.5,1.4,0.2]})
+    response = client.post("/predict", json={"features":[5.1,3.5,1.4,0.2]})
     assert response.status_code == 200
