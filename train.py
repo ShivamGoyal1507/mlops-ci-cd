732c917f2571a96c8c46c8f60a850c84e243d956
ShivamGoyal1507 <2023aib1016@iitrpr.ac.in>
2026-03-10 19:31:36 +0530

    model change to random forest

--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 import joblib
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
 
 data = load_iris()
 
@@ -10,10 +10,9 @@ y = data.target
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-model = LogisticRegression(max_iter=200)
+model = RandomForestClassifier(max_iter=200)
 
 model.fit(X_train, y_train)
 
 joblib.dump(model, "model.pkl")
-
 print("Model saved as model.pkl")
