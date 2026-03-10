0751a27b5456bb23d057d7d6472305c5bcb0c74d
ShivamGoyal1507 <2023aib1016@iitrpr.ac.in>
2026-03-10 11:38:01 +0530

    mlops pipeline project

---
+++ app.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+import joblib
+import numpy as np
+
+app = FastAPI()
+
+model = joblib.load("model.pkl")
+
+@app.get("/")
+def home():
+    return {"message": "ML Model API Running"}
+
+@app.post("/predict")
+def predict(data: dict):
+    features = np.array(data["features"]).reshape(1, -1)
+    prediction = model.predict(features)
+    return {"prediction": int(prediction[0])}
