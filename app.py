d2e23d61eae0a9a7f50fcb7adae674dc682eeaca
ShivamGoyal1507 <2023aib1016@iitrpr.ac.in>
2026-04-08 13:37:11 +0530

    app.py updated

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ model = joblib.load("model.pkl")
 
 @app.get("/")
 def home():
-    return {"message": "ML Model API Running.Your model is ready to make predictions."}
+    return {"message": "ML Model API Running.Your model is ready."}
 
 @app.post("/predict")
 def predict(data: dict):
